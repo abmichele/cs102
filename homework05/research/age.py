3d7d6805dceea80415524209ae7dfe38c5dbb56e
abmichele <70940211+abmichele@users.noreply.github.com>
2021-01-29 21:50:23 +0300

    Add files via upload

---
+++ homework05/research/age.py
@@ -0,0 +1,28 @@
+import datetime as dt
+import statistics
+import typing as tp
+
+from vkapi.friends import get_friends
+from dateutil.relativedelta import relativedelta
+
+def age_predict(user_id: int) -> tp.Optional[float]:
+    """
+    Наивный прогноз возраста пользователя по возрасту его друзей.
+
+    Возраст считается как медиана среди возраста всех друзей пользователя
+
+    :param user_id: Идентификатор пользователя.
+    :return: Медианный возраст пользователя.
+    """
+    ages: tp.List[float] = []
+    friends = get_friends(user_id, fields=["bdate"])
+    for friend in friends.items:
+        try:
+            birth_date = dt.datetime.strptime(friend["bdate"], "%d.%m.%Y")
+            age = relativedelta(dt.datetime.now(), birth_date).years
+            ages.append(age)
+        except (ValueError, KeyError):
+            pass
+    if not ages:
+        return None
+    return statistics.median(ages)
