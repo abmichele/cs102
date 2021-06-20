82b17956e7f5eac5712e71fa0f3d3c345fac594c
abmichele <70940211+abmichele@users.noreply.github.com>
2021-06-21 00:16:11 +0300

    Add files via upload

--- homework06/db.py
+++ homework06/db.py
@@ -1,22 +1,67 @@
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import typing as tp
 
+from hw06.scraputils import get_news
+from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy import Column, String, Integer
 
 Base = declarative_base()
-engine = create_engine("sqlite:///news.db")
-session = sessionmaker(bind=engine)
+path_news_db = "sqlite:///news.db"
+engine = create_engine(path_news_db, connect_args={"check_same_thread": False})
+local_session = sessionmaker(autocommit=False, autoflush=False)
 
 
-class News(Base):
+class News(Base):  # type: ignore
     __tablename__ = "news"
     id = Column(Integer, primary_key=True)
     title = Column(String)
     author = Column(String)
     url = Column(String)
-    comments = Column(Integer)
     points = Column(Integer)
     label = Column(String)
 
+
+@tp.no_type_check
+def get_session(engine: Engine) -> Session:
+    local_session.configure(bind=engine)
+    return local_session()
+
+
+def make_table_news(session: Session, news: tp.List[tp.Dict[str, tp.Union[int, str]]]) -> None:
+    for i in range(len(news)):
+        means = News(
+            title=news[i]["title"],
+            author=news[i]["author"],
+            url=news[i]["url"],
+            points=news[i]["points"],
+        )
+        session.add(means)
+    session.commit()
+
+
+@tp.no_type_check
+def change_label(session: Session, id: int, label: str) -> None:
+    item = session.query(News).get(id)
+    item.label = label
+    session.commit()
+
+
+def get_new_news(session: Session, url: str = "https://news.ycombinator.com/newest") -> None:
+    news = get_news(url)
+    news_news = []
+    for something in news:
+        main, name = something["title"], something["author"]
+        if not list(session.query(News).filter(News.title == main, News.author == name)):
+            news_news.append(something)
+    make_table_news(session, news_news)
+
+
 Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    make_table_news(
+        get_session(engine), get_news(url="https://news.ycombinator.com/newest", n_pages=4)
+    )
--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,36 +1,60 @@
-from bottle import (
-    route, run, template, request, redirect
-)
+import re
+import string
+import typing as tp
 
-from scrapper import get_news
-from db import News, session
-from bayes import NaiveBayesClassifier
+from bottle import redirect, request, route, run, template
+from hw06.bayes import NaiveBayesClassifier
+from hw06.database import News, change_label, engine, get_new_news, get_session
+from sqlalchemy.sql.expression import label
 
-
+@tp.no_type_check
+@route("/")
 @route("/news")
 def news_list():
-    s = session()
+    s = get_session(engine)
     rows = s.query(News).filter(News.label == None).all()
-    return template('news_template', rows=rows)
+    return template("news_template", rows=rows)
 
 
+@tp.no_type_check
 @route("/add_label/")
 def add_label():
-    # PUT YOUR CODE HERE
+    s = get_session(engine)
+    id = request.query["id"]
+    label = request.query["label"]
+    change_label(s, id, label)
     redirect("/news")
 
 
+@tp.no_type_check
 @route("/update")
 def update_news():
-    # PUT YOUR CODE HERE
+    s = get_session(engine)
+    get_new_news(s)
     redirect("/news")
 
 
+colors = {"good": "#00d2fe", "never": "#180f3b", "maybe": "#969696"}
+
+
+@tp.no_type_check
 @route("/classify")
 def classify_news():
-    # PUT YOUR CODE HERE
+    s = get_session(engine)
+    model = NaiveBayesClassifier()
+    train_set = s.query(News).filter(News.label != None).all()
+    model.fit([clean(news.title).lower() for news in train_set], [news.label for news in train_set])
+    test = s.query(News).filter(News.label == None).all()
+    cell = list(map(lambda x: model.predict(x.title), test))
+    return template(
+        "color_template", rows=list(map(lambda x: (x[1], colors[cell[x[0]]]), enumerate(test)))
+    )
+
+
+def clean(s: str) -> str:
+    translator = str.maketrans("", "", string.punctuation)
+    return s.translate(translator)
 
 
 if __name__ == "__main__":
     run(host="localhost", port=8080)
-
