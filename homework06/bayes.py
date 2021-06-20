5402dfd5d41016cedc6f8a80f4c9d2fc8861a183
abmichele <70940211+abmichele@users.noreply.github.com>
2021-06-21 00:25:44 +0300

    Update bayes.py

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -4,14 +4,14 @@ class NaiveBayesClassifier:
         pass
 
     def fit(self, X, y):
-        """ Fit Naive Bayes classifier according to X, y. """
+        """Fit Naive Bayes classifier according to X, y."""
         pass
 
     def predict(self, X):
-        """ Perform classification on an array of test vectors X. """
+        """Perform classification on an array of test vectors X."""
         pass
 
     def score(self, X_test, y_test):
-        """ Returns the mean accuracy on the given test data and labels. """
+        """Returns the mean accuracy on the given test data and labels."""
         pass
 
